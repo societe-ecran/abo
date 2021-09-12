982fa88ce91ee654e6879434be59caa7ef51fae4
societe-ecran <antoninreigneaud@gmail.com>
2021-09-12 21:46:06 +0200

    snipcar v2 test

--- components/seo/Seo.js
+++ components/seo/Seo.js
@@ -26,8 +26,8 @@ const Seo = (props) => {
             <link rel="preconnect" href="<https://cdn.snipcart.com>" /> */}
 
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-        <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key={process.env.SNIPCART_API} id="snipcart"></script>
-        <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" rel="stylesheet" type="text/css" />
+            <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key={process.env.SNIPCART_API} id="snipcart"></script>
+            <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" rel="stylesheet" type="text/css" />
 
         </Head>
     )
