a115c61e2318b95c797f8141e94f102aca1ae67a
societe-ecran <antoninreigneaud@gmail.com>
2021-09-12 20:55:34 +0200

    snipcart test

--- pages/leLivre.js
+++ pages/leLivre.js
@@ -60,7 +60,7 @@ const leLivre = () => {
                                 data-item-id="livre"
                                 data-item-price="14"
                                 data-item-weight="494"
-                                data-item-url="/"
+                                data-item-url="/leLivre"
                                 data-item-description="Paru le 10 septembre 2021. 336 pages.  ISBN: 979-10-96195-14-5 "
                                 // data-item-image="../public/couv.jpg"
                                 data-item-name="Abolir la police. Échos des États-Unis">
