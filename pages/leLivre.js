5303c7d1547dd7a7aafeb18b933129fa1b97239e
societe-ecran <antoninreigneaud@gmail.com>
2021-09-12 20:24:50 +0200

    snipcart class

--- pages/leLivre.js
+++ pages/leLivre.js
@@ -54,7 +54,7 @@ const leLivre = () => {
                             <div className=" md:hidden">
                     <div>
                         <div className="pt-6 pb-6">
-                            <button class="snipcart-add-item rounded-full py-2 px-4 violetBack text-white  "
+                            <button className="snipcart-add-item rounded-full py-2 px-4 violetBack text-white  "
                                 data-item-id="livre"
                                 data-item-price="14"
                                 data-item-weight="494"
