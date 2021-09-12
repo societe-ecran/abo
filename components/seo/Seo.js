1ffbe22b52cf7621c413e1f0ad0164d043c6e305
societe-ecran <antoninreigneaud@gmail.com>
2021-09-12 21:41:35 +0200

    snipcar v2

--- components/seo/Seo.js
+++ components/seo/Seo.js
@@ -19,18 +19,15 @@ const Seo = (props) => {
             <link rel="icon" href="/favicon.ico" />
 
 
-            <script src="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.js"></script>
-            <div hidden id="snipcart" data-api-key={process.env.SNIPCART_API} ></div>
+            {/* <script src="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.js"></script>
+            <div hidden id="snipcart" data-api-key={process.env.SNIPCART_API} ></div> */}
             {/* <link rel="stylesheet" href="<https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.css>" /> */}
-            <link rel="preconnect" href="<https://app.snipcart.com>" />
-            <link rel="preconnect" href="<https://cdn.snipcart.com>" />
+            {/* <link rel="preconnect" href="<https://app.snipcart.com>" />
+            <link rel="preconnect" href="<https://cdn.snipcart.com>" /> */}
 
-            {/* <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-            <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-
-            <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key={process.env.SNIPCART_API} id="snipcart"></script>
-
-            <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" rel="stylesheet" type="text/css" /> */}
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+        <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key="MDJjY2U2MmQtYjEyMS00ODM3LWI5Y2YtMjE2MTQ3NGY3ZmJhNjM3NjYzODU4NjEyMDk5Nzc2" id="snipcart"></script>
+        <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" rel="stylesheet" type="text/css" />
 
         </Head>
     )
