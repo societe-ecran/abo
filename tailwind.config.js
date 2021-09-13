4370bd213da131847e83d4f51c04a64c85821955
societe-ecran <antoninreigneaud@gmail.com>
2021-09-13 16:55:03 +0200

    snipcar v2 test encore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-  mode: 'jit',
+  // mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: theme => ({
-
-        'degrad-left': "url('/degrad-left.webp')",
-
-        'degrad-top': "url('/degrad-top.webp')",
-       })
 
     },
   },
