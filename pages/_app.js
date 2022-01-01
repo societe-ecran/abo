196620623bc60e1464e41985e0b4d99e2fbd2afd
societe-ecran <antoninreigneaud@gmail.com>
2022-01-01 19:07:22 +0100

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return(
   <>
    <Script
-        strategy='lazyOnload'
+        strategy='afterInteractive'
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
       />
       <Script id='ga-analytics'>
