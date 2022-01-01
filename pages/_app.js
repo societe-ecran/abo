2b4ef66ca9669a5ff44e63aa78fe6171bc97ad0f
societe-ecran <antoninreigneaud@gmail.com>
2022-01-01 18:52:57 +0100

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
 // import 'tailwindcss/tailwind.css'
 import '../public/styles/globalStyle.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+  <>
+   <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script id='ga-analytics'>
+        {
+          `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `
+        }
+      </Script>
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
